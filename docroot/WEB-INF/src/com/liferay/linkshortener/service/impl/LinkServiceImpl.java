544330aeada3b23df4dd547d5578bcae85b4ca6f
Miroslav Ligas <miroslav.ligas@liferay.com>
2015-01-08 15:27:36 +0000

    LRDCOM-13102 added remote service

--- docroot/WEB-INF/src/com/liferay/linkshortener/service/impl/LinkServiceImpl.java
+++ docroot/WEB-INF/src/com/liferay/linkshortener/service/impl/LinkServiceImpl.java
@@ -14,7 +14,18 @@
 
 package com.liferay.linkshortener.service.impl;
 
+import com.liferay.linkshortener.NoSuchLinkException;
+import com.liferay.linkshortener.ShortLinkTakenException;
+import com.liferay.linkshortener.model.Link;
 import com.liferay.linkshortener.service.base.LinkServiceBaseImpl;
+import com.liferay.linkshortener.service.util.ShortURLUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+
+import java.util.Date;
+import java.util.List;
+
+import static com.liferay.linkshortener.util.ApplicationConstants.AUTO_SHORTEN_PREFIX;
 
 /**
  * The implementation of the link remote service.
@@ -39,5 +50,99 @@ public class LinkServiceImpl extends LinkServiceBaseImpl {
 	 *
 	 * Never reference this interface directly. Always use {@link com.liferay.linkshortener.service.LinkServiceUtil} to access the link remote service.
 	 */
+
+	/**
+	 * Method checks if the short link is already used. If the link is not used
+	 * it adds a new link to database. Also notifies the appropriate model
+	 * listeners.
+	 *
+	 * @param  link the link
+	 * @return the link that was added
+	 * @throws com.liferay.portal.kernel.exception.SystemException if a system exception occurred
+	 */
+	public Link addLink(Link link)
+		throws ShortLinkTakenException, SystemException {
+
+		return linkLocalService.addLinkWithCheck(link);
+	}
+
+	/**
+	 * Deletes the link with the primary key from the database. Also notifies the appropriate model listeners.
+	 *
+	 * @param linkId the primary key of the link
+	 * @return the link that was removed
+	 * @throws PortalException if a link with the primary key could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
+	public Link deleteLink(long linkId) throws PortalException, SystemException {
+		return linkLocalService.deleteLink(linkId);
+	}
+
+	/**
+	 * Returns the link with the primary key.
+	 *
+	 * @param linkId the primary key of the link
+	 * @return the link
+	 * @throws PortalException if a link with the primary key could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
+	public Link getLink(long linkId) throws PortalException, SystemException {
+		return linkLocalService.getLink(linkId);
+	}
+
+	/**
+	 * Method loads links auto-generated or explicitly created links from
+	 * database. Method supports paging.
+	 *
+	 * @param  autoGen specifies what kind of links should be loaded
+	 * @param  start the lower bound of the range of links
+	 * @param  end the upper bound of the range of links (not inclusive)
+	 * @return the range of matching links
+	 * @throws SystemException if a system exception occurred
+	 */
+	public List<Link> getLinksByAutoGen(boolean autoGen, int start, int end)
+		throws SystemException {
+
+		return linkLocalService.getLinksByAutoGen(autoGen, start, end);
+	}
+
+	/**
+	 * Method returns the link which short link matches the provided value.
+	 *
+	 * @param  shortLink the short link
+	 * @return the matching link
+	 * @throws com.liferay.linkshortener.NoSuchLinkException if a matching link
+	 *         could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
+	public Link getLinksByShortLink(String shortLink)
+		throws NoSuchLinkException, SystemException {
+
+		return linkLocalService.getLinksByShortLink(shortLink);
+	}
+
+	/**
+	 * Factory method for a new Link object.
+	 *
+	 * @return new object
+	 */
+	public Link linkFactory() {
+		return linkLocalService.linkFactory();
+	}
+
+	/**
+	 * Method checks if the short link is already taken if it was modified. If
+	 * the link is free it updates the entry. Also notifies the appropriate
+	 * model listeners.
+	 *
+	 * @param  link the link
+	 * @return the link that was updated
+	 * @throws SystemException if a system exception occurred
+	 */
+	public Link updateLink(Link link)
+		throws ShortLinkTakenException, SystemException {
+
+		return linkLocalService.updateLinkWithCheck(link);
+	}
 
 }
